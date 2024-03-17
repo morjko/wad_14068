b6ab4e34f88731c35ba3f5f00d143a93d7acd6b0
morjko <mar1nalee2003@yandex.com>
2024-03-17 15:39:28 +0500

    Add project files.

---
+++ ContactManager/Controllers/GroupsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Data;
+using ContactManager.Models;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly ContactManagerDBContext _context;
+
+        public GroupsController(ContactManagerDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groups
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> Getgroups()
+        {
+          if (_context.groups == null)
+          {
+              return NotFound();
+          }
+            return await _context.groups.ToListAsync();
+        }
+
+        // GET: api/Groups/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Group>> GetGroup(int id)
+        {
+          if (_context.groups == null)
+          {
+              return NotFound();
+          }
+            var @group = await _context.groups.FindAsync(id);
+
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return @group;
+        }
+
+        // PUT: api/Groups/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroup(int id, Group @group)
+        {
+            if (id != @group.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@group).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroupExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Groups
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Group>> PostGroup(Group @group)
+        {
+          if (_context.groups == null)
+          {
+              return Problem("Entity set 'ContactManagerDBContext.groups'  is null.");
+          }
+            _context.groups.Add(@group);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGroup", new { id = @group.Id }, @group);
+        }
+
+        // DELETE: api/Groups/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGroup(int id)
+        {
+            if (_context.groups == null)
+            {
+                return NotFound();
+            }
+            var @group = await _context.groups.FindAsync(id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            _context.groups.Remove(@group);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GroupExists(int id)
+        {
+            return (_context.groups?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ContactManager/Data/ContactManagerDBContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data
+{
+    public class ContactManagerDBContext : DbContext
+    {
+        public ContactManagerDBContext(DbContextOptions<ContactManagerDBContext> options) : base(options) { }
+
+        public DbSet<Group> groups { get; set; }
+    }
+}
